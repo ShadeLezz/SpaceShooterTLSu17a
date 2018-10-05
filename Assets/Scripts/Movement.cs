98a04cfb6619838a0d541464e503966a5e4c228a
Tobias Andren <tobias.andren@elev.ga.lbs.se>
2018-10-05 11:43:24 +0200

    bu

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
-
-    private Rigidbody2D rb2d;
+   
 	// Use this for initialization
 	void Start ()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+      
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        float moveVertical = Input.GetAxis("Vertical");
+   
 	}
 }
