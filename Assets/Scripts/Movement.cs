925854f2fb5500133261a1a48dbc254f9d7394a5
Tobias Andren <tobias.andren@elev.ga.lbs.se>
2018-10-10 13:35:54 +0200

    Movement script :)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-   
-	// Use this for initialization
-	void Start ()
+    public float speed;
+
+
+
+    // public float turnSpeed;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
     {
-      
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-   
-	}
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+    }
 }
