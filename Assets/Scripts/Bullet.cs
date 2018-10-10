71221cb56f9b358093e26eaa2f0152c1c70bc032
Lucas Andreasson <Lucas.andreasson@elev.ga.lbs.se>
2018-10-10 14:46:06 +0200

    Shooting scripts
    uwu

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo);
+        Destroy(gameObject);
+    }
+}
